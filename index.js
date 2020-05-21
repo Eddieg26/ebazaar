2fb1c2863b5e05693dca6826627b72993f63c96a
Eddie Goodman <eddie_0226@outlook.com>
2020-05-21 18:34:20 -0400

    set up server and client

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+// const routes = require('./routes/index.route');
+const cors = require('cors');
+const path = require("path");
+
+require('dotenv').config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI || process.env.DB_URI, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+    .then(client => {
+
+        app.use(express.static(path.join(__dirname, "client", "build")))
+
+        app.use(bodyParser.urlencoded({ extended: true }));
+
+        app.use(bodyParser.json());
+
+        app.use(cors());
+
+        // app.use('/api/', routes);
+
+        app.get("*", (req, res) => {
+            res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`listening on port ${process.env.PORT}`);
+        });
+    })
+    .catch(console.error);
