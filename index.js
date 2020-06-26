68093f339eb86f9eb9adb8785514836d351e4a04
Eddie Goodman <eddie_0226@outlook.com>
2020-06-26 19:52:59 -0400

    small change

--- index.js
+++ index.js
@@ -23,17 +23,10 @@ mongoose.connect(process.env.MONGODB_URI || process.env.DB_URI, { useUnifiedTopo
         app.use('/api/', routes);
 
         if (process.env.NODE_ENV === 'production') {
-            // Serve any static files
-            // app.use(express.static(path.join(__dirname, 'client/build')));
-            // Handle React routing, return all requests to React app
-            // app.get('*', function (req, res) {
-            //     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-            // });
+            app.use(express.static(path.join(__dirname, "client", "build")))
 
-            app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
-
-            app.get("/*", function (req, res) {
-                res.sendFile(path.join(__dirname, "index.html"));
+            app.get("*", (req, res) => {
+                res.sendFile(path.join(__dirname, "client", "build", "index.html"));
             });
         }
 
