af6045ad181ba3183c85fc78dc16ad6b837c0c54
Eddie Goodman <eddie_0226@outlook.com>
2020-06-26 19:33:40 -0400

    addes some production code

--- index.js
+++ index.js
@@ -22,6 +22,15 @@ mongoose.connect(process.env.MONGODB_URI || process.env.DB_URI, { useUnifiedTopo
 
         app.use('/api/', routes);
 
+        if (process.env.NODE_ENV === 'production') {
+            // Serve any static files
+            app.use(express.static(path.join(__dirname, 'client/build')));
+            // Handle React routing, return all requests to React app
+            app.get('*', function (req, res) {
+                res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+            });
+        }
+
         app.listen(process.env.PORT, () => {
             console.log(`listening on port ${process.env.PORT}`);
         });
